fb8a1ac9d90ea311bc542c5e7782d320c6f93d81
Syahril <everaldo@umich.edu>
2014-06-27 22:44:02 +0800

    Finishied signin and signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate( params[:session][:password])
-      #Code to redirect user to show page when successful
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = "Invalid email/password combination!"
       render 'static_pages/home'
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    sign_out
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome new shopper!"
+      sign_in @user
+      flash[:success] = "Welcome, #{@user.name}!"
       redirect_to @user
     else
       render 'new'
