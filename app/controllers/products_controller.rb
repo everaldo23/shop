7be0e07b8e3d5f0c3b5aa28f0da548f9c655c104
Syahril <everaldo@umich.edu>
2014-07-05 17:16:25 +0800

    Finally got modal working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,11 @@ class ProductsController < ApplicationController
   
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+        format.html
+        format.js
+        format.json { render json: @product}
+    end
   end
   
   def create
